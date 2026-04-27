dd318bbe5437d48a95670c23523552ff82fcffb9
Hannah GRace Tabuan <hannahgtabuan@gmail.com>
2026-04-27 10:41:16 -0700

    first origin

--- file&error.py
+++ file&error.py
@@ -7,3 +7,29 @@ def main():
             raise ValueError("Username cannot be empty.")
         if age <= 0:
             raise ValueError("Age must be a positive number.")
+        with open("users.txt", "a") as file:
+            file.write(f"{username} - {age}\n")
+
+        print("\nSaved successfully!\n")
+
+    except ValueError as e:
+        print("Error:", e)
+
+    except Exception as e:
+        print("Unexpected error:", e)
+
+    finally:
+
+        try:
+            print("\n--- Saved Users ---")
+            with open("users.txt", "r") as file:
+                for line in file:
+                    print(line.strip())
+        except FileNotFoundError:
+            print("No users saved yet.")
+
+        print("\nSystem complete.")
+
+
+
+main()
